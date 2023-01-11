38df62ba317d897e4d00f3bc2641c91525f6c05b
rincedal <302313054@student.rocmondriaan.nl>
2023-01-11 12:20:10 +0100

    Intial commit

---
+++ Templates/GameBase_Homepage.php
@@ -0,0 +1,22 @@
+<?php
+include_once('/Users/rizot/Documents/GitHub/sd21-p6-reviewyourexperience-gamebase/Templates/defaults/head.php');
+include_once('/Users/rizot/Documents/GitHub/sd21-p6-reviewyourexperience-gamebase/Templates/defaults/navbar.php');
+?>
+
+<!DOCTYPE HTML>
+    <body>
+
+
+        <img class= "position-relative bannerimage" src="/Public/Images/GameBase_Banner_Image.jpg">
+        <img class= "position-relative bannerimage2" src="/Public/Images/uasupport.jpg">
+        <a href= "https://war.ukraine.ua/"><button type="button" class="btn btn-primary button1 position-relative">Meer info</button></a>
+
+
+
+
+
+
+       <?php  include_once('/Users/rizot/Documents/GitHub/sd21-p6-reviewyourexperience-gamebase/Templates/defaults/footer.php'); ?>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous"></script>
+    </body>
+
