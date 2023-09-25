45240df5e7a0ecaa039bc6671025eb2f2286d6e9
rincedal <94070451+Diomuzan247@users.noreply.github.com>
2023-09-25 12:35:13 +0200

    Transformed GameBase into console app

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # GameBase
+GameBase is a C# and .NET console application which is lastly made for a fictive gaming blog company called GameBase.
--- Templates/GameBase_Detail.php
+++
@@ -1,9 +0,0 @@
-<!DOCTYPE HTML>
-<head>
-    <?php include_once('/Users/rizot/Documents/GitHub/GameBase/Templates/defaults/GameBase_Head.php'); ?>
-</head>
-<body>
-<?php include_once('/Users/rizot/Documents/GitHub/GameBase/Templates/defaults/GameBase_Navbar.php'); ?>
-
-
-</body>
